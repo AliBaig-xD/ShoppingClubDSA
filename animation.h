9049e52f0c7db3ebc2a5fbc7999e4452b61a6a99
AliBaig-xD <69807734+AliBaig-xD@users.noreply.github.com>
2021-01-04 19:14:49 +0500

    Add files via upload

---
+++ animation.h
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+COORD coord = {0, 0};
+void gotoxy(int x, int y)
+{
+	COORD coord;
+	coord.X = x;
+	coord.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+}
